0b8a9971e6bd2593fe5f71dbc8cee3fcdf5473ee
unknown <=>
2010-05-12 20:32:53 +0200

    first blog commit

---
+++ application/logs/2010/05/12.php
@@ -0,0 +1,3 @@
+<?php defined('SYSPATH') or die('No direct script access.'); ?>
+
+2010-05-12 11:59:20 --- ERROR: ReflectionException [ -1 ]: Class controller_blog does not exist ~ SYSPATH/classes\kohana\request.php [ 864 ]
